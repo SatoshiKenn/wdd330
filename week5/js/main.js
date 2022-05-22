f353e1a9d9e47909f5b0bf53bc2525ee328b95f7
Kennette Guevara <92239496+SatoshiKenn@users.noreply.github.com>
2022-05-22 01:17:44 -0400

    week 5 update

---
+++ week5/js/main.js
@@ -0,0 +1,6 @@
+import Hikes from "./hike.js";
+//on load grab the array and insert it into the page
+
+const myHike = new Hikes('hikeListId');
+
+myHike.showHikeList();
--- week5/js/mainw5.js
+++ week5/js/mainw5.js
@@ -7,24 +7,12 @@ p_year.innerHTML = `&copy; ${year}  :|: Kennette Guevara :|: SV-CL`;
 //Assignment links
 const links = [
     {
-        label: "Week 1",
-        url: "week1/index.html"
+        label: "W05 Team Activity",
+        url: "teamactivity.html"
     },
     {
-        label: "Week 2",
-        url: "week2/index.html"
-    },
-    {
-        label: "Week 3",
-        url: "week3/index.html"
-    },
-    {
-        label: "Week 4",
-        url: "week4/index.html"
-    },
-    {
-        label: "Week 5",
-        url: "week5/index.html"
+        label: "Return to Portfolio",
+        url: "../index.html"
     }
 ]
 
