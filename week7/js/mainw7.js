9ca9cbae69a0dd0c7df583e850beae3645b45f2c
Kennette Guevara <92239496+SatoshiKenn@users.noreply.github.com>
2022-06-05 01:50:52 -0400

    week 7 update

--- week7/js/mainw7.js
+++ week7/js/mainw7.js
@@ -7,32 +7,12 @@ p_year.innerHTML = `&copy; ${year}  :|: Kennette Guevara :|: SV-CL`;
 //Assignment links
 const links = [
     {
-        label: "Week 1",
-        url: "week1/index.html"
+        label: "Hikes",
+        url: "hiking.html"
     },
     {
-        label: "Week 2",
-        url: "week2/index.html"
-    },
-    {
-        label: "Week 3",
-        url: "week3/index.html"
-    },
-    {
-        label: "Week 4",
-        url: "week4/index.html"
-    },
-    {
-        label: "Week 5",
-        url: "week5/index.html"
-    },
-    {
-        label: "Week 6",
-        url: "week6/index.html"
-    },
-    {
-        label: "Week 7",
-        url: "week7/index.html"
+        label: "Return to Portfolio",
+        url: "../index.html"
     }
 ]
 
