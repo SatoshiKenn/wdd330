73a8406263d93f8b8a927f5a6c5928cd2116afdd
Kennette Guevara <92239496+SatoshiKenn@users.noreply.github.com>
2022-06-25 20:50:10 -0400

    week 10 update

---
+++ week10/js/index.js
@@ -0,0 +1,5 @@
+import QuakesController from './QuakesController.js';
+import buildNavigation from './routing.js';
+
+const navElement = document.getElementById('mainNav');
+buildNavigation(navElement);
---
+++ week10/js/mainw10.js
@@ -0,0 +1,35 @@
+//Get year code
+const p_year = document.querySelector("#copyright");
+const today = new Date();
+const year = today.getFullYear();
+p_year.innerHTML = `&copy; ${year}  :|: Kennette Guevara :|: SV-CL`;
+
+//Assignment links
+const links = [
+    {
+        label: "Team Activity",
+        url: "teamactivity.html"
+    },
+    {
+        label: "Return to Portfolio",
+        url: "../index.html"
+    }
+]
+
+//Display links in the portal
+const list = document.querySelector("#list");
+
+function displayLinks(link) {
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+    
+    li.setAttribute("class", "liList");
+    a.setAttribute("class", "aList");
+    a.setAttribute("href", `${link.url}`)
+    a.innerHTML = `${link.label}`;
+
+    li.appendChild(a);
+    list.appendChild(li);
+}
+
+links.forEach(displayLinks);
